f069cd451bad9c3b87c1fd0f1a8627f38c755f98
AndyAlis <lisligi@mail.ru>
2022-06-22 17:20:44 +0300

    start weather app

---
+++ weather.js
@@ -0,0 +1,8 @@
+const apiKey = "0ffa5a62469461b23ba6b8586a9aa0c9";
+
+document.querySelector('input[type="text"]').addEventListener('keyup', event => {
+    const phrase = event.target.value;
+    console.log(phrase);
+})
+
+// https://youtu.be/J4WSmVX6Lhg?t=570
